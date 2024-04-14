7c3d24c620db825d03674ad34c5110058a3915d1
belov <167007150+leonidBELO@users.noreply.github.com>
2024-04-14 16:58:41 +0300

    ssshahahhah
    elhgu5hrtufhxuhgy7egyuhuiudshruwhuihgdughrugtyrtdsertyukiyresgjkytrtjkuyjky,

---
+++ youtube_contest.py
@@ -0,0 +1,48 @@
+#from pyqt5.qtcore import ot
+from pyqt5.qtWidgets import qapplicationb.ob
+from pyqt5.qtWidgets 
+
+#создание приложения и главного окна
+ 
+#создание виджетов главного окна
+app = qapplication({})
+main_win = Qwidget()
+main_win.setwindowtitle
+puesttion = 
+btn_answer1=QRadiubutton
+btn_answer1=QRadiubutton(Я даже незнаю)
+btn_answer1=QRadiubuntton
+
+               
+#отображение окна приложения 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+\
+
+
+
+
+
+
+
+
+
+
